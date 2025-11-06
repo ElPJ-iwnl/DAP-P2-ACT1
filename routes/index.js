cd2110f96dbf5ec127a0ff18e7659aa8db181b47
PedroJC9 <pedrojalil10@gmail.com>
2025-11-06 18:51:09 -0500

    nuevo aumento de galeria - fotos v2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Mi Álbum con Bootstrap' });
+});
+
+router.get('/photos', async (req, res, next) => {
+  try {
+    const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json';
+
+    const { data } = await axios.get(URL);
+
+    const fotos = Array.isArray(data) ? data.filter(Boolean) : Object.values(data || {});
+    res.render('fotos', { title: 'Fotos', fotos });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
